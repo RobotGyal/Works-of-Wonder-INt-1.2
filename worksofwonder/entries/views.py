b5ad7668da0c32df6ef8119f19d19ccb8b5c7499
Aleia Knight <aleiaknight@gmail.com>
2020-01-07 14:13:35 -0500

    laying out views

--- worksofwonder/entries/views.py
+++ worksofwonder/entries/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'base.html')
+
+def readers(request):
+    return render(request, 'readers.html')
+
+def writers(request):
+    return render(request, 'writers.html')
+
+def login(request):
+    return render(request, 'login.html')
