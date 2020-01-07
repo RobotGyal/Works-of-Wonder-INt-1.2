69754a370d5b091ae8a14e9c77ebb5d762f3f2dc
Aleia Knight <aleiaknight@gmail.com>
2020-01-07 15:58:30 -0500

    superuser creation

--- worksofwonder/users/views.py
+++ worksofwonder/users/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
-
+from django.contrib import messages
 # Create your views here.
 def signup(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username} ')
+            return redirect('index')
+    else:
+        form = UserCreationForm()
     return render(request, 'users/signup.html', {'form': form})
