8552f65fc56825a1b0f9beb0effda302f369c668
Aleia Knight <aleiaknight@gmail.com>
2020-01-07 15:49:53 -0500

    user signup form

--- worksofwonder/worksofwonder/urls.py
+++ worksofwonder/worksofwonder/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from entries import views
+from users import views as user_views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('account/signup/',user_views.signup, name='signup'),
     path('readers/', views.readers, name='readers'),
     path('admin/', admin.site.urls),
 ]
