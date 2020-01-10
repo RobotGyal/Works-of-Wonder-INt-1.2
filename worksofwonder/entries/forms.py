61c12e43014df8ca8c909ef9366e805ce49922b9
Aleia Knight <aleiaknight@gmail.com>
2020-01-09 19:19:55 -0800

    upload form

--- worksofwonder/entries/forms.py
+++ worksofwonder/entries/forms.py
@@ -1,5 +1,16 @@
 from django import forms
+from django.contrib.auth.models import User
+
 
 class ArticleForm(forms.Form):
-    username = forms.CharField(max_length=100, label='username')
-    content = forms.Textarea()
+    author = forms.CharField(max_length=100, label='author')
+    content = forms.CharField(widget=forms.Textarea(attrs={'width':"100%", 'cols' : "80", 'rows': "20", }))
+    email= forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = [
+            'author',
+            'content',
+            'email'
+        ]
--- worksofwonder/entries/views.py
+++ worksofwonder/entries/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect 
+from django.contrib import messages
 from django.http import HttpResponse
 from .models import Articles
+from .forms import ArticleForm
 
 
 # Create your views here.
@@ -20,5 +22,13 @@ def login(request):
     return render(request, 'login.html')
 
 def upload(request):
-    return render(request, 'entries/upload.html')
-
+    if request.method == 'POST':
+        upload_form = ArticleForm(request.POST)
+        if  upload_form.is_valid():
+            upload_form.save()
+            username =  upload_form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username} ')
+            return redirect('index')
+    else:
+            upload_form = ArticleForm()
+    return render(request, 'entries/upload.html', {'upload_form': upload_form})
