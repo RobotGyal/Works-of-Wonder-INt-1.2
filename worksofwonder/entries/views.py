8fc9aef927209eed8e934c7b34943713da906307
Aleia Knight <aleiaknight@gmail.com>
2020-01-09 17:13:04 -0800

    adding models for databasing

--- worksofwonder/entries/views.py
+++ worksofwonder/entries/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Articles
+
 
 # Create your views here.
 def index(request):
     return render(request, 'entries/index.html')
 
 def readers(request):
-    return render(request, 'entries/readers.html')
+    context={
+        'items':Articles.objects.all()
+    }
+    return render(request, 'entries/readers.html',context)
 
 def writers(request):
     return render(request, 'entries/writers.html')
